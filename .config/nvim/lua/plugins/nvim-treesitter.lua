a96181902e4217b03e1ca3c85e9f051889d5d12d
Jess Bodzo <jess.bodzo@ditto.com>
2025-06-26 13:21:20 -0400

    chore: tweak treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -14,10 +14,18 @@ return {
     end
 
     nvim_treesitter.setup {
-      ensure_installed = { "bash", "cpp", "css", "html", "javascript", "json", "vim", "lua", "rust", "toml", "python", "typescript", "yaml", "go", "starlark" },
+      branch = 'master',
+      lazy = false,
+      build = ":TSUpdate",
+      ensure_installed = { "bash", "cpp", "css", "javascript", "json", "vim", "lua", "rust", "toml", "python", "typescript", "yaml", "go", "starlark" },
       auto_install = true,
       highlight = {
         enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting=false,
       },
       ident = { enable = true },
